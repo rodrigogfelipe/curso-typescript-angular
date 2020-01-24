6235be0725b1f78987b7b5b0a92d7ed7aff9a52b
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-24 13:38:43 -0300

    Variaveis

---
+++ app.ts
@@ -0,0 +1,12 @@
+let message: string= "Ola mundo"
+console.log(message)
+
+let episodio: number = 4;
+console.log("Quantos episodios" + 4)
+
+episodio = episodio + 1
+console.log("Episodio e" + episodio)
+
+let favorito
+favorito= '88-8'
+console.log("Meu favorito" + favorito)
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+var message = "Ola mundo";
+console.log(message);
+var episodio = 4;
+console.log("Quantos episodios" + 4);
+episodio = episodio + 1;
+console.log("Episodio e" + episodio);
+var favorito;
+favorito = '88-8';
+console.log("Meu favorito" + favorito);
