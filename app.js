6b1d5160094b1579aff889b632cc3d52ec3253c5
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-24 22:40:51 -0300

    Usando funcoes - 09

--- app.js
+++ app.js
@@ -7,18 +7,8 @@ console.log("Episodio e" + episodio);
 var favorito;
 favorito = '88-8';
 console.log("Meu favorito" + favorito);
-/*Declarando funções*/
 var isEnoughtToBeatMF = function (parsecs) {
     return parsecs < 12;
 };
-var distance = 11;
+var distance = 14;
 console.log("Is " + distance + " parsecs enought to be Millenium Falcom ? " + (isEnoughtToBeatMF(distance) ? 'YES' : 'NO'));
-/*Função padrao do typescript*/
-var call = function (name) { return console.log("Do you copy, " + name + " ?"); };
-call('R2');
-function inc(speed, inc) {
-    if (inc === void 0) { inc = 1; }
-    return speed + inc;
-}
-console.log("inc (5,1) = " + inc(5, 1));
-console.log("inc (5,1) = " + inc(5));
--- app.ts
+++ app.ts
@@ -10,3 +10,23 @@ console.log("Episodio e" + episodio)
 let favorito
 favorito= '88-8'
 console.log("Meu favorito" + favorito)
+
+/*Declarando funções*/
+let isEnoughtToBeatMF= function(parsecs: number): boolean {
+    return parsecs < 12
+}
+
+let distance= 11
+console.log(`Is ${distance} parsecs enought to be Millenium Falcom ? ${isEnoughtToBeatMF(distance) ? 'YES' : 'NO'}`)
+
+/*Função padrao do typescript*/
+let call = (name: string) => console.log(`Do you copy, ${name} ?`)
+call('R2')
+
+function inc (speed:number, inc: number = 1) : number {
+    return speed + inc
+}
+
+/*Os dois consoles declarados imprimira o valor igual a  6*/
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5,1) = ${inc(5)}`)
